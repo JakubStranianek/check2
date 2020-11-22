10d366484d150a280ff839cb2957bc6d65c87ce1
BuildTools <unconfigured@null.spigotmc.org>
2020-11-22 02:08:02 +0100

    praca s databazou pridanie mena priezviska studenta + gombiky pre vymazanie a edit cize update

--- inc/add-new.php
+++ inc/add-new.php
@@ -1,1 +1,15 @@
 <?php
+
+require_once 'config.php';
+
+if (isset($database)) {
+    $id = $database->insert("stud", [
+        "Name" => $_POST['messageN'],
+        "Surname" => $_POST['messageS']
+    ]);
+}
+
+if ($id) {
+    header('Location: /check2');
+    die();
+}
--- index.php
+++ index.php
@@ -5,23 +5,34 @@
 </div>
 
 <?php if (isset($database)) {
-    $data = $database->select('stud', 'Name');
-}   ?>
+    $data = $database->select("stud", "*");
+} ?>
 
 <div class="row">
     <div class="col-8">
         <ul class="list-group pt-3">
-            <?php foreach ((array)$data as $item) {
-                echo '<li class="list-group-item">'.$item.'</li>';
-            } ?>
+            <?php
+            foreach($data as $item)
+            {
+                echo    '<li class="list-group-item">';
+                echo    $item["ID"]." . ";
+                echo    $item["Name"] . " ";
+                echo    $item["Surname"];
+                echo    '<div class="controls pull-right">';
+                echo        '<a href="edit.php?id=" class="edit-link">Edit</a>';
+                echo        '<a href="delete.php?id=" class="delete-link">X</a>';
+                echo    '</div>';
+                echo    '</li>';
+            }
+             ?>
         </ul>
     </div>
 
     <div class="col-4">
         <form class="col-sm-9 pt-3" action="inc/add-new.php" method="post">
             <p class="form-group">
-                <textarea class="form-control" name="message" id="text" rows="1" placeholder="is there [ Zadajte meno ]"></textarea>
-                <textarea class="form-control" name="message" id="text" rows="1" placeholder="is there [ Zadajte priezvisko ]"></textarea>
+                <textarea class="form-control" name="messageN" id="text" rows="1" placeholder="is there [ Zadajte meno ]"></textarea>
+                <textarea class="form-control" name="messageS" id="text" rows="1" placeholder="is there [ Zadajte priezvisko ]"></textarea>
             </p>
 
             <p class="form-group">
